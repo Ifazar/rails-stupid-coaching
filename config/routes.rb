a3f9e92458fa0defb4ad45704b2f8005b2224162
RAZAFIMANOHIHAJA Aurelien <aurelienrazafi@gmail.com>
2023-11-13 13:47:27 +0100

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'questions#ask'
 
+  get 'answer', to: 'questions#answer'
+  get 'ask', to: 'questions#ask'
   # Defines the root path route ("/")
   # root "articles#index"
 end
